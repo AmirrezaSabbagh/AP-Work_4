f8c8fffa44527e4b3527383d8c97e3c9c8ae78c7
kasra <kasra.re138406@gmail.com>
2024-04-10 13:37:16 +0330

    added constructor to Vote

--- Vote.java
+++ Vote.java
@@ -1,5 +1,8 @@
 public class Vote {
     private final Person voter;
     private final String date;
-        
+    public vote(Person voter  , String date){
+        this.date = date;
+        this.voter = voter;
+    }        
 }
