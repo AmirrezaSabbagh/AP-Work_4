10eb13858398499e093de3ec774982a6d2cece14
kasra <kasra.re138406@gmail.com>
2024-04-10 17:48:17 +0330

    add getters to VotingSystem

--- VotingSystem.java
+++ VotingSystem.java
@@ -1,7 +1,17 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class VotingSystem {
     private ArrayList<Voting> votingList = new ArrayList<>();
+
     public VoatingSystem(){}
     
+    public Voting getVoting(int index) {
+        return votingList.get(index);
+    }
+
+    public ArrayList<Voting> getVotingList() {
+        return votingList;
+    }
 }
